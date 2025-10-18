f55e7eae0610b09a4317b959523500f6f2e0ed1a
Phil B <muppetbrown@gmail.com>
2025-10-18 13:37:00 +1300

    index fix

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -5,7 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Root element not found');
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
