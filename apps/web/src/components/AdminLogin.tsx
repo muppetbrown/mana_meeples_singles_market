5a92e78a3c332f69d7cee8ef7032280534488e5d
Phil B <muppetbrown@gmail.com>
2025-10-20 20:08:24 +1300

    remove redundant React import

--- apps/web/src/components/AdminLogin.tsx
+++ apps/web/src/components/AdminLogin.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Lock, User, AlertCircle } from 'lucide-react';
 import { api } from '@/config/api';
