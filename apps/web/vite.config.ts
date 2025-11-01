071708b6d8065899ed26ae2880c7fac93e8781e3
Phil B <muppetbrown@gmail.com>
2025-11-01 15:24:19 +1300

    render deploy fix

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,5 +1,5 @@
 // apps/web/vite.config.ts
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
